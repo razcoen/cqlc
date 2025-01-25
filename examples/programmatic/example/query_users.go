238db5f620a69228e91a6e2101b4596b80250b22
Raz Cohen <raz@icohens.com>
2025-01-25 13:17:55 +0200

    test(example): include batch insert and delete as well as find many

--- examples/programmatic/example/query_users.go
+++ examples/programmatic/example/query_users.go
@@ -29,6 +29,63 @@ func (c *Client) CreateUser(ctx context.Context, params *CreateUserParams, opts 
 	return nil
 }
 
+type CreateUsersParams struct {
+	UserID    gocql.UUID
+	Username  string
+	Email     string
+	CreatedAt time.Time
+}
+
+func (c *Client) CreateUsers(ctx context.Context, params []*CreateUsersParams, opts ...gocqlc.BatchOption) error {
+	b := c.session.NewBatch(gocql.UnloggedBatch)
+	for _, v := range params {
+		b.Query("INSERT INTO users (user_id, username, email, created_at) VALUES (?, ?, ?, ?)", v.UserID, v.Username, v.Email, v.CreatedAt)
+	}
+	b = b.WithContext(ctx)
+	for _, opt := range opts {
+		b = opt.Apply(b)
+	}
+	if err := c.session.ExecuteBatch(b); err != nil {
+		return fmt.Errorf("exec batch: %w", err)
+	}
+	return nil
+}
+
+type DeleteUserParams struct {
+	UserID gocql.UUID
+}
+
+func (c *Client) DeleteUser(ctx context.Context, params *DeleteUserParams, opts ...gocqlc.QueryOption) error {
+	q := c.session.Query("DELETE FROM users WHERE user_id = ?", params.UserID)
+	q = q.WithContext(ctx)
+	for _, opt := range opts {
+		q = opt.Apply(q)
+	}
+	if err := q.Exec(); err != nil {
+		return fmt.Errorf("exec query: %w", err)
+	}
+	return nil
+}
+
+type DeleteUsersParams struct {
+	UserID gocql.UUID
+}
+
+func (c *Client) DeleteUsers(ctx context.Context, params []*DeleteUsersParams, opts ...gocqlc.BatchOption) error {
+	b := c.session.NewBatch(gocql.UnloggedBatch)
+	for _, v := range params {
+		b.Query("DELETE FROM users WHERE user_id = ?", v.UserID)
+	}
+	b = b.WithContext(ctx)
+	for _, opt := range opts {
+		b = opt.Apply(b)
+	}
+	if err := c.session.ExecuteBatch(b); err != nil {
+		return fmt.Errorf("exec batch: %w", err)
+	}
+	return nil
+}
+
 type FindUserParams struct {
 	UserID gocql.UUID
 }
@@ -51,4 +108,78 @@ func (c *Client) FindUser(ctx context.Context, params *FindUserParams, opts ...g
 		return nil, fmt.Errorf("scan row: %w", err)
 	}
 	return &result, nil
+}
+
+type FindUsersParams struct {
+	Email string
+}
+
+type FindUsersResult struct {
+	UserID    gocql.UUID
+	CreatedAt time.Time
+	Email     string
+	Username  string
+}
+
+type FindUsersQuerier struct {
+	query  *gocql.Query
+	logger gocqlc.Logger
+}
+
+func (q *FindUsersQuerier) All(ctx context.Context) ([]*FindUsersResult, error) {
+	var results []*FindUsersResult
+	var pageState []byte
+	for {
+		page, err := q.Page(ctx, pageState)
+		if err != nil {
+			return nil, fmt.Errorf("page: %w", err)
+		}
+		results = append(results, page.Results()...)
+		if len(page.PageState()) == 0 {
+			break
+		}
+		pageState = page.PageState()
+	}
+	return results, nil
+}
+
+type FindUsersResultsPage struct {
+	results   []*FindUsersResult
+	pageState []byte
+	numRows   int
+}
+
+func (page *FindUsersResultsPage) Results() []*FindUsersResult { return page.results }
+func (page *FindUsersResultsPage) NumRows() int                { return page.numRows }
+func (page *FindUsersResultsPage) PageState() []byte           { return page.pageState }
+
+func (q *FindUsersQuerier) Page(ctx context.Context, pageState []byte) (*FindUsersResultsPage, error) {
+	var results []*FindUsersResult
+	iter := q.query.WithContext(ctx).PageState(pageState).Iter()
+	defer func() {
+		if err := iter.Close(); err != nil {
+			q.logger.Error("iter.Close() returned with error", "error", err)
+		}
+	}()
+	nextPageState := iter.PageState()
+	scanner := iter.Scanner()
+	for scanner.Next() {
+		var result FindUsersResult
+		if err := scanner.Scan(&result.UserID, &result.CreatedAt, &result.Email, &result.Username); err != nil {
+			return nil, fmt.Errorf("scan result: %w", err)
+		}
+		results = append(results, &result)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+	return &FindUsersResultsPage{results: results, pageState: nextPageState, numRows: iter.NumRows()}, nil
+}
+
+func (c *Client) FindUsers(params *FindUsersParams, opts ...gocqlc.QueryOption) *FindUsersQuerier {
+	q := c.session.Query("SELECT * FROM users WHERE email = ? ALLOW FILTERING", params.Email)
+	for _, opt := range opts {
+		q = opt.Apply(q)
+	}
+	return &FindUsersQuerier{query: q, logger: c.logger}
 }
--- examples/programmatic/example_test.go
+++ examples/programmatic/example_test.go
@@ -12,39 +12,238 @@ import (
 )
 
 func TestExample(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("create 2 users and find one by one", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+		err = client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+
+		result1, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID1})
+		require.NoError(t, err)
+		require.Equal(t, example.FindUserResult{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		}, *result1)
+
+		result2, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID2})
+		require.NoError(t, err)
+		require.Equal(t, example.FindUserResult{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		}, *result2)
+	})
+
+	t.Run("create 2 users and find one of many", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+		err = client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+
+		querier := client.FindUsers(&example.FindUsersParams{Email: "test_email_1"})
+		results, err := querier.All(ctx)
+		require.NoError(t, err)
+		require.Len(t, results, 1)
+		require.Equal(t, example.FindUsersResult{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		}, *results[0])
+	})
+
+	t.Run("create 2 users and find both", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+		err = client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+
+		querier := client.FindUsers(&example.FindUsersParams{Email: "test_email_1"})
+		results, err := querier.All(ctx)
+		require.NoError(t, err)
+		require.Len(t, results, 2)
+		require.ElementsMatch(t, []*example.FindUsersResult{
+			{
+				UserID:    userID1,
+				Username:  "test_user_1",
+				Email:     "test_email_1",
+				CreatedAt: createdAt,
+			},
+			{
+				UserID:    userID2,
+				Username:  "test_user_2",
+				Email:     "test_email_1",
+				CreatedAt: createdAt,
+			},
+		}, results)
+	})
+
+	t.Run("batch create 2 users and find both", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUsers(ctx, []*example.CreateUsersParams{
+			{
+				UserID:    userID1,
+				Username:  "test_user_1",
+				Email:     "test_email_1",
+				CreatedAt: createdAt,
+			},
+			{
+				UserID:    userID2,
+				Username:  "test_user_2",
+				Email:     "test_email_2",
+				CreatedAt: createdAt,
+			},
+		})
+		require.NoError(t, err)
+
+		result1, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID1})
+		require.NoError(t, err)
+		require.Equal(t, example.FindUserResult{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		}, *result1)
+		result2, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID2})
+		require.NoError(t, err)
+		require.Equal(t, example.FindUserResult{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		}, *result2)
+	})
+
+	t.Run("create 2 users and delete 1 and find the other", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+		err = client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+
+		err = client.DeleteUser(ctx, &example.DeleteUserParams{UserID: userID1})
+		require.NoError(t, err)
+
+		result1, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID1})
+		require.ErrorIs(t, err, gocql.ErrNotFound)
+		require.Nil(t, result1)
+
+		result2, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID2})
+		require.NoError(t, err)
+		require.Equal(t, example.FindUserResult{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		}, *result2)
+	})
+
+	t.Run("create 2 users and batch delete both", func(t *testing.T) {
+		client := newClientWithRandomKeyspace(t)
+
+		userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
+		createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
+		err := client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID1,
+			Username:  "test_user_1",
+			Email:     "test_email_1",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+		err = client.CreateUser(ctx, &example.CreateUserParams{
+			UserID:    userID2,
+			Username:  "test_user_2",
+			Email:     "test_email_2",
+			CreatedAt: createdAt,
+		})
+		require.NoError(t, err)
+
+		err = client.DeleteUsers(ctx, []*example.DeleteUsersParams{{UserID: userID1}, {UserID: userID2}})
+		require.NoError(t, err)
+
+		result1, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID1})
+		require.ErrorIs(t, err, gocql.ErrNotFound)
+		require.Nil(t, result1)
+		result2, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID2})
+		require.ErrorIs(t, err, gocql.ErrNotFound)
+		require.Nil(t, result2)
+	})
+}
+
+func newClientWithRandomKeyspace(t *testing.T) *example.Client {
 	session, _ := testcassandra.ConnectWithRandomKeyspace(t)
 	testcassandra.Exec(t, session, "schema.cql")
-
 	client, err := example.NewClient(session, nil)
 	require.NoError(t, err)
-	defer func() { require.NoError(t, client.Close()) }()
-
-	ctx := context.Background()
-	userID1 := gocql.UUID(uuid.Must(uuid.NewUUID()))
-	createdAt := time.UnixMilli(time.Now().UnixMilli()).UTC() // Cassandra only keeps milliseconds precision on timestamps
-	err = client.CreateUser(ctx, &example.CreateUserParams{
-		UserID:    userID1,
-		Username:  "test_user_1",
-		Email:     "test_email_1",
-		CreatedAt: createdAt,
-	})
-	require.NoError(t, err)
-
-	userID2 := gocql.UUID(uuid.Must(uuid.NewUUID()))
-	err = client.CreateUser(ctx, &example.CreateUserParams{
-		UserID:    userID2,
-		Username:  "test_user_2",
-		Email:     "test_email_2",
-		CreatedAt: createdAt,
-	})
-	require.NoError(t, err)
-
-	result, err := client.FindUser(ctx, &example.FindUserParams{UserID: userID1})
-	require.NoError(t, err)
-	require.Equal(t, example.FindUserResult{
-		UserID:    userID1,
-		Username:  "test_user_1",
-		Email:     "test_email_1",
-		CreatedAt: createdAt,
-	}, *result)
+	t.Cleanup(func() { require.NoError(t, client.Close()) })
+	return client
 }
